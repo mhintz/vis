dc5cf37784b74a2a8b4fbf59c286e9dae9f9ab8c
Mark <markohintz@gmail.com>
2013-08-21 18:31:57 -0700

    fiddling with particles

--- vis.js
+++ vis.js
@@ -1,11 +1,14 @@
 (function(root) {
 
+	/*** INTERNALS ***/
 	var VIS = {
-		version: "0.0.1",
+		_version: "0.0.1",
 		width: 0,
 		height: 0,
-		installed: false
+		TWO_PI: 2 * Math.PI
 	};
+
+	root.VIS = VIS;
 
 	// TODO: add support for node? windowed mode?
 	// see https://github.com/rogerwang/node-webkit for probable way to do this
@@ -13,7 +16,15 @@
 	var canvas = document.createElement("canvas");
 	var ctx = canvas.getContext("2d");
 
-	internalSetup();
+	if (typeof root.onload !== undefined) {
+		var _load = root.onload;
+		root.onload = function() {
+			if (typeof _load === "function") _load();
+			internalSetup();
+		};
+	} else {
+		internalSetup();
+	}
 
 	function internalSetup() {
 		if (typeof setup !== "undefined") setup();
@@ -25,27 +36,21 @@
 
 	function internalLoop() {
 		if (typeof update !== "undefined") update();
-		else VIS.update();
 		if (typeof draw !== "undefined") draw();
-		else VIS.draw();
 
 		// TODO: throttle loop speed to some framerate
 
 		requestAnimationFrame(internalLoop);
 	}
 
+	/*** SETUP ***/
 	VIS.install = function() {
 		for (var name in VIS) {
-			if (typeof VIS[name] === "function") {
+			if (name.slice(0, 1) !== "_") {
 				root[name] = VIS[name];
 			}
 		}
-		VIS.installed = true;
 	};
-
-	VIS.update = function() {};
-
-	VIS.draw = function() {};
 
 	VIS.setCanvas = function(newCanvas) {
 		canvas = newCanvas;
@@ -59,34 +64,193 @@
 	VIS.size = function(width, height) {
 		VIS.width = width;
 		canvas.setAttribute("width", width);
+		root.width = width;
 		VIS.height = height;
 		canvas.setAttribute("height", height);
+		root.height = height;
 	};
 
+	/*** DRAW SETTINGS ***/
 	VIS.background = function(r, g, b) {
 		if (arguments.length === 1) g = b = r;
+		var _fill = ctx.fillStyle;
 		ctx.fillStyle = VIS.rgbToHex(r, g, b);
 		ctx.fillRect(0, 0, VIS.width, VIS.height);
-		ctx.fillStyle = "";
+		ctx.fillStyle = _fill;
+	};
+
+	VIS.stroke = function(r, g, b) {
+		if (arguments.length === 1) g = b = r;
+		ctx.strokeStyle = VIS.rgbToHex(r, g, b);
+		VIS.stroke = true;
+	};
+
+	VIS.lineWidth = function(w) {
+		ctx.lineWidth = w;
+	};
+
+	VIS.lineCap = function(cap) {
+		switch (cap) {
+			case "butt":
+			case "round":
+			case "square":
+				ctx.lineCap = cap;
+			break;
+		}
+	};
+
+	VIS.lineJoin = function(join) {
+		switch (join) {
+			case "round":
+			case "bevel":
+			case "miter":
+				ctx.lineJoin = join;
+			break;
+		}
+	};
+
+	VIS.noStroke = function() {
+		ctx.strokeStyle = "";
+		VIS.stroke = false;
 	};
 
 	VIS.fill = function(r, g, b) {
 		if (arguments.length === 1) g = b = r;
 		ctx.fillStyle = VIS.rgbToHex(r, g, b);
+		VIS.fill = true;
 	};
 
 	VIS.noFill = function() {
 		ctx.fillStyle = "";
+		VIS.fill = false;
 	};
 
+	/*** SHAPE PRIMITIVES ***/
 	VIS.rect = function(x, y, w, h) {
-		if (ctx.fillStyle) {
-			ctx.fillRect(x, y, w, h);
-		} else {
-			ctx.rect(x, y, w, h);
-		}
+		if (VIS.stroke) ctx.strokeRect(x, y, w, h);
+		if (VIS.fill) ctx.fillRect(x, y, w, h);
+		else ctx.rect(x, y, w, h);
 	};
 
+	VIS.triangle = function(x1, y1, x2, y2, x3, y3) {
+		ctx.beginPath();
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+		ctx.lineTo(x3, y3);
+		ctx.lineTo(x1, y1);
+		if (VIS.stroke) ctx.stroke();
+		if (VIS.fill) ctx.fill();
+	};
+
+	VIS.circle = function(x, y, r) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.arc(x, y, r, 0, VIS.TWO_PI);
+		if (VIS.stroke) ctx.stroke();
+		if (VIS.fill) ctx.fill();
+	};
+
+	/*** DATA CLASSES ***/
+	VIS.Point = function(x, y) {
+		this.x = x;
+		this.y = y;
+	};
+
+	VIS.Point.prototype = (function() {
+		var proto = {};
+
+		proto.reset = function(x, y) {
+			this.x = x;
+			this.y = y;
+		};
+
+		return proto;
+	})();
+
+	VIS.Vec2D = function(x, y) {
+		this.x = x;
+		this.y = y;
+	};
+
+	VIS.Vec2D.prototype = (function() {
+		var proto = VIS.Point.prototype;
+
+		proto.add = function(pt) {
+			this.x += pt.x;
+			this.y += pt.y;
+		};
+
+		proto.rot = function(pt) {
+			this.x *= pt.x;
+			this.y *= pt.y;
+		};
+
+		return proto;
+	})();
+
+	VIS.Particle = function(loc, vel, acc) {
+		this.loc = loc;
+		this.vel = vel;
+		this.acc = acc;
+	};
+
+	VIS.Particle.prototype = (function() {
+		var proto = {};
+
+		proto.loc = function(loc) {
+			if (arguments.length === 0) return this.loc;
+			this.loc = loc;
+		};
+
+		proto.vel = function(vel) {
+			if (arguments.length === 0) return this.vel;
+			this.vel = vel;
+		};
+
+		proto.acc = function(acc) {
+			if (arguments.length === 0) return this.acc;
+			this.acc = acc;
+		};
+
+		proto.update = function() {
+			this.vel.add(this.acc);
+			this.loc.add(this.vel);
+		};
+
+		proto.draw = function() {
+			VIS.circle(this.loc.x, this.loc.y, 1);
+		};
+
+		proto.inView = function() {
+			return this.loc.x >= 0 && this.loc.x <= VIS.width && this.loc.y >= 0 && this.loc.y <= VIS.height;
+		};
+
+		return proto;
+	})();
+
+	VIS.Triangle = function(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	};
+
+	VIS.Triangle.prototype = (function() {
+		var proto = {};
+
+		proto.reset = function(a, b, c) {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		};
+
+		proto.draw = function() {
+			VIS.triangle(this.a.x, this.a.y, this.b.x, this.b.y, this.c.x, this.c.y);
+		};
+
+		return proto;
+	})();
+
+	/*** COLOR UTILITIES ***/
 	VIS.rgbToHex = function(r, g, b) {
 		r = r.toString(16), g = g.toString(16), b = b.toString(16);
 		if (r.length < 2) r = "0" + r;
@@ -95,6 +259,12 @@
 		return "#" + r + g + b;
 	};
 
-	root.VIS = VIS;
+	/*** UTILITIES ***/
+	VIS.random = function(low, high) {
+		if (arguments.length === 1) high = low, low = 0;
+		return Math.random() * (high - low) + low;
+	};
+
+
 
 })(this);
